a7a38106aada6edc12bb997025e2d1ba528b3d6b
Isha Bisht <isha.bisht.2781@gmail.com>
2026-07-18 17:31:25 +0530

    Add user login and jwt generation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv'
+dotenv.config();
 
 export const registerUser = async (req, res) => {
   try {
@@ -9,14 +11,16 @@ export const registerUser = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({
         success: false,
-        message:"Email already exists"
-      })
+        message: "Email already exists",
+      });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
-      name, email, password: hashedPassword
+      name,
+      email,
+      password: hashedPassword,
     });
 
     await user.save();
@@ -24,7 +28,6 @@ export const registerUser = async (req, res) => {
       success: true,
       message: "User registered successfully",
     });
-
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -32,3 +35,43 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+
+export const loginUser = async (req, res) => {
+  
+
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "Invalid email or password",
+    });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+
+    return res.status(401).json({
+      success: false,
+      message: "Invalid email or password",
+    });
+    
+  }
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "login successful",
+    token: token,
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+  });
+}
