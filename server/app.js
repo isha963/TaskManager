cd7b9736a41673a1a9f343b74b3c3c7791336c00
Isha Bisht <isha.bisht.2781@gmail.com>
2026-07-22 17:27:03 +0530

    Implement login functionality with React Context authentication

--- server/app.js
+++ server/app.js
@@ -3,13 +3,20 @@ import indexRoutes from "./routes/indexRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import userRoutes from "./routes/userRoutes.js"
 import errorHandler from './middleware/errorMiddleware.js';
+import cors from "cors";
+
 
 const app = express();
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use("/", indexRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api", userRoutes);
 app.use(errorHandler);
 export default app;
