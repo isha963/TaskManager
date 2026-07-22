5a691646d58e3982ebf750dc89c024cb8f62ee4c
Isha Bisht <isha.bisht.2781@gmail.com>
2026-07-22 15:50:13 +0530

    Set up authentication context and persistence

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect,useState } from "react";
 
 const AuthContext = createContext();
 
@@ -6,15 +6,30 @@ export default function AuthProvider({ children }) {
   
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
+  
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    const storedUser = localStorage.getItem("user");
+
+    if (storedToken && storedUser) {
+      setToken(storedToken);
+      setUser(JSON.parse(storedUser));
+    }
+  })
 
   const login = (userdata, jwtToken) => {
     setUser(userdata);
     setToken(jwtToken);
+
+    localStorage.setItem("token", jwtToken);
+    localStorage.setItem("user", JSON.stringify(userdata));
   }
 
   const logout = () => {
     setUser(null);
     setToken(null);
+     localStorage.removeItem("token");
+     localStorage.removeItem("user");
   };
 
   return (
